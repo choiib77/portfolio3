5d4a52e40f9497ca6d46daa16f19d275ee5e9628
choiib77 <choiib77@gmail.com>
2023-11-01 22:59:35 +0900

    231101 - css 수정

--- js/index.js
+++ js/index.js
@@ -20,8 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
 const index_wrapper = document.querySelector('.index_wrapper');
 const video1 = document.querySelector('.section2 video')
 const sec2_video_wrap = document.querySelector('.sec2_video_wrap')
+const sec2_txt_wrap_li = document.querySelectorAll('.sec2_txt_wrap ul li');
 index_wrapper.addEventListener('scroll',()=>{
+    var scrollY = index_wrapper.scrollTop;
+    var innerHeight = window.innerHeight;
+    var scrollHeight = document.body.offsetHeight;
+    var sec2_txt_wrap_ul = document.querySelector('.sec2_txt_wrap ul').offsetTop;
+    const middleOfViewport = window.innerHeight / 2 + window.scrollY;
+    // 동영상 크기 함수
     animateV();
+
+    // index_wrapper 안의 위치값기준으로 스크롤값 구함
+    const sec2_video_wrap_s = sec2_video_wrap.getBoundingClientRect();
+    const sec2_video_wrap_s2 = sec2_video_wrap_s.top;
+    if(sec2_video_wrap_s2 <= 130){
+        document.querySelector('.sec2_txt_wrap').classList.add("ani");
+    }else{
+        document.querySelector('.sec2_txt_wrap').classList.remove("ani");
+    }
+    // 스크롤시 색상 진해짐
+    sec2_txt_wrap_li.forEach(function(item, index) {
+        const itemTop = item.getBoundingClientRect().top + window.scrollY;
+        const itemBottom = itemTop + item.clientHeight;
+        
+        if (itemTop <= middleOfViewport + item.clientHeight && itemBottom >= middleOfViewport) {
+            item.classList.add('opacity');
+        } else {
+            item.classList.remove('opacity');
+        }
+    });
+    
 });
 
 const sec2_text1 = document.querySelector('.sec2_video_txt1');
@@ -37,3 +65,4 @@ function animateV(){
     textTrans = textTrans < 0 ? 0 : textTrans;
     sec2_text1.style.transform = `translateX(${-textTrans}px)`;
 }
+
